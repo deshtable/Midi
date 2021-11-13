922cfee378a39d02b7977af297d90b406c5ff38e
Ahmad Bajwa <AIB190004@utdallas.edu>
2021-11-13 15:21:20 -0600

    Final Setup

--- Arpeggiator.java
+++ Arpeggiator.java
@@ -6,14 +6,14 @@ public class Arpeggiator {
 
     public void runArpeggio(int typeArpeggio){
 
-        if(typeArpeggio ==1)
+        if(typeArpeggio == 1)  // If the type of arpeggio is chosen to be a major chord
             majorArpeggio();
 
     }
 
     private void majorArpeggio() {
 
-
+        //
 
     }
 
---
+++ midiExport.java
@@ -0,0 +1,14 @@
+public class midiExport {
+
+    String [] arpeggArr;
+
+    public midiExport(){
+
+    }
+
+    public midiExport(String[] arr){
+        //takes in the string of notes and produces midi based on that
+    }
+
+
+}
