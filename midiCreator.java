e20ea085306a9769e9140765e86888891bddac0f
Desh Padmakumar <deshspo@gmail.com>
2021-11-13 17:04:10 -0600

    working midi ouput

--- midiCreator.java
+++ midiCreator.java
@@ -7,18 +7,25 @@
  * author  Karl Brown
  * last updated 2/24/2003
  */
+import java.util.ArrayList; // import the ArrayList class
 
 import java.io.*;
 import java.util.*;
 import javax.sound.midi.*; // package for all midi classes
-public class midifile
-{
+public class midiCreator{
   public static void main(String argv[]) {
     System.out.println("midifile begin ");
-	try
-	{
+
+    constructor("c", "c");
+
+
+}
 //****  Create a new MIDI sequence with 24 ticks per beat  ****
-		Sequence s = new Sequence(javax.sound.midi.Sequence.PPQ,24);
+	public static void constructor(String note, String key){
+
+    try{
+
+    Sequence s = new Sequence(javax.sound.midi.Sequence.PPQ,24);
 
 //****  Obtain a MIDI track from the sequence  ****
 		Track t = s.createTrack();
@@ -61,66 +68,52 @@ public class midifile
 		mm.setMessage(0xC0, 0x00, 0x00);
 		me = new MidiEvent(mm,(long)0);
 		t.add(me);
-//////////////////////////////////////////////
 
+    String[] noteClassifier = {"c","c#","d","d#","e","f","f#","g","g#", "a", "a#", "b"};
+    //byte noteByte;
+    String noteByteString;
+    ArrayList<String> noteList = new ArrayList<String>(Arrays.asList("c","c#","d","d#","e","f","f#","g","g#", "a", "a#", "b"));
+     // Create an ArrayList object
+    int noteByte;
+    String[] arpeggArr = {"c", "e", "g"};
+    int startCount = 1;
+    int endCount = 120;
+    for( int i =0; i< arpeggArr.length; i++){
+      mm = new ShortMessage();
+      //noteByteString = ()(noteList.indexOf(arpeggArr[i]) + 60));
+      //System.out.println(noteByteString.getBytes());
+      //noteByte = 0x42;
+      noteByte = noteList.indexOf(arpeggArr[i]) + 60;
+      mm.setMessage(0x90,noteByte,0x60);
+      me = new MidiEvent(mm,(long)startCount);
+      t.add(me);
 
+    //****  note off - middle C - 120 ticks later  ****
+      mm = new ShortMessage();
+      mm.setMessage(0x80,noteByte,0x40);
+      me = new MidiEvent(mm,(long)endCount);
+      t.add(me);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//****  note on - middle C  ****
-		mm = new ShortMessage();
-		mm.setMessage(0x90,0x3C,0x60);
-		me = new MidiEvent(mm,(long)1);
-		t.add(me);
-
-//****  note off - middle C - 120 ticks later  ****
-		mm = new ShortMessage();
-		mm.setMessage(0x80,0x3C,0x40);
-		me = new MidiEvent(mm,(long)121);
-		t.add(me);
-//****  note on - middle C  ****
-		mm = new ShortMessage();
-		mm.setMessage(0x90,0x3D,0x60);
-		me = new MidiEvent(mm,(long)122);
-		t.add(me);
-
-//****  note off - middle C - 120 ticks later  ****
-		mm = new ShortMessage();
-		mm.setMessage(0x80,0x3C,0x40);
-		me = new MidiEvent(mm,(long)244);
-		t.add(me);
-
-
-
-
-//****  set end of track (meta event) 19 ticks later  ****
+      System.out.println(noteByte);
+      System.out.println(arpeggArr[i]);
+      startCount += 120;
+      endCount += 120;
+    }
+    //****  set end of track (meta event) 19 ticks later  ****
 		mt = new MetaMessage();
         byte[] bet = {}; // empty array
 		mt.setMessage(0x2F,bet,0);
-		me = new MidiEvent(mt, (long)260);
+		me = new MidiEvent(mt, (long)(endCount+1));
 		t.add(me);
-
-//****  write the MIDI sequence to a MIDI file  ****
+    //****  write the MIDI sequence to a MIDI file  ****
 		File f = new File("midifile.mid");
 		MidiSystem.write(s,1,f);
-	} //try
-		catch(Exception e)
-	{
-		System.out.println("Exception caught " + e.toString());
-	} //catch
-    System.out.println("midifile end ");
-  } //main
+
+    System.out.println("midifile end "); //main
+
+  }
+    catch(Exception e){
+      System.out.println("Exception caught " + e.toString());
+    }
+  }
 } //midifile
