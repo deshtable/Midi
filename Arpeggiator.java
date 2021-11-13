26467c627bf94c4a20cb86888c4c1bbccda8230c
Ahmad Bajwa <AIB190004@utdallas.edu>
2021-11-13 15:10:15 -0600

    Basic Project Setup

---
+++ Arpeggiator.java
@@ -0,0 +1,20 @@
+public class Arpeggiator {
+
+    public Arpeggiator(){
+
+    }
+
+    public void runArpeggio(int typeArpeggio){
+
+        if(typeArpeggio ==1)
+            majorArpeggio();
+
+    }
+
+    private void majorArpeggio() {
+
+
+
+    }
+
+}
