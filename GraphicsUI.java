1caed8b116a1180c621b536d22d430e793a6ca8f
akshayjagadeesh <cbeakshay@gmail.com>
2021-11-13 19:19:00 -0600

    graphics resize

--- GraphicsUI.java
+++ GraphicsUI.java
@@ -1,16 +1,49 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class GraphicsUI extends JPanel implements MouseListener {
+public class GraphicsUI extends JFrame implements MouseListener {
 
     String [] arpeggArr;
 
+    private JButton btn;
+    private int xcoor, ycoor, width, height;
 
     public GraphicsUI (){
+        setSize(1280, 720);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    public void paint(Graphics g) {
+
+        width = getSize().width;
+        height = getSize().height;
+        //setSize(getSize().width, getSize().height);
+
+        drawKeys(g);
+
 
     }
 
+    private void drawKeys(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+        int drawx = width /3;
+        int drawy = height /3;
+        int sizex = width /25;
+        int sizey = height /2;
+        g.setColor(Color.BLACK);
+        for(int i=1;i<=7;i++)
+            g.drawRect(drawx+i*sizex, drawy, sizex, sizey);
+        int tempsize= (sizex/3)*2;
+
+        g.fillRect(drawx+tempsize+sizex, drawy, tempsize, sizey/2);
+        g.fillRect(drawx+tempsize+2*sizex, drawy, tempsize, sizey/2);
+        g.fillRect(drawx+tempsize+4*sizex, drawy, tempsize, sizey/2);
+        g.fillRect(drawx+tempsize+5*sizex, drawy, tempsize, sizey/2);
+        g.fillRect(drawx+tempsize+6*sizex, drawy, tempsize, sizey/2);
+    }
 
     private void readFiles() {
     }
